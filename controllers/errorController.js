2692a495eddf49ff67bb3cf08def239a69bd463c
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-03 18:16:25 -0400

    feat: add dev error and prod error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (error, response) => {
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    error: error,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (error, response) => {
+  if (error.isOperational) {
+    response.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.error('ERROR', error);
+
+    response.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 export default (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  response.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(error, response);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, response);
+  }
 };
