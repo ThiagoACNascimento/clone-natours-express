73c110e890bf1b055aa4ca49868d3558c9e9ba11
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-07 22:24:34 -0400

    feat: use nodemailer and create `natoursEmail` to send emails

---
+++ utils/email.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+
+const send = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'Test Application <noreply@testapplication.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+const natoursEmail = {
+  send,
+};
+
+export default natoursEmail;
