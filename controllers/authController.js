6b5dd8a84c225ca240e5430a9568dd4c96d59255
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-09 20:14:59 -0400

    feat: add sending `JWT` via `Cookie`

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,17 @@ function signToken(id) {
 
 function createSendToken(user, statusCode, response, isReturnUser) {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+  };
+
+  response.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   if (isReturnUser)
     return response.status(statusCode).json({
