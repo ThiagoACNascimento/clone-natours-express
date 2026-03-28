122d136df40230b0e833bec4651112dea32c0a3f
Thiago Costa <thiagoacnascimento@gmail.com>
2026-03-28 20:21:53 -0300

    feat: add `JSON` files to import their data

---
+++ database/scripts/import-dev-data.js
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import '../../config.env.js';
+import mongoose from 'mongoose';
+import Tour from '../../models/tourModel.js';
+import Review from '../../models/reviewModel.js';
+import User from '../../models/userModel.js';
+
+mongoose
+  .connect(
+    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  )
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync('database/data/tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('database/data/users.json', 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync('database/data/reviews.json', 'utf-8'),
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
