41d892b755527d06abe5c07627a43c9acbda36ab
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-03 12:58:59 -0300

    feat: add calc of average tour rating when delete or update a review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -73,14 +73,30 @@ reviewSchema.statics.calcAverageRatings = async function (tour) {
     },
   ]);
 
-  await Tour.findByIdAndUpdate(tour._id, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tour._id, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tour._id, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 reviewSchema.post('save', function () {
   this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.result = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.result.constructor.calcAverageRatings(this.result.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
