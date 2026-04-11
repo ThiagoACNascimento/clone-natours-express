4126b30d2c163723b360ad57e7332638d545040a
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-11 15:08:52 -0300

    feat: add docs for `viewRoutes.js`

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,3 +7,49 @@ viewRouter.get('/', viewController.getOverview);
 viewRouter.get('/tour/:slug', viewController.getTour);
 
 export default viewRouter;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Views
+ *   description: Rotas que renderizam páginas HTML
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Renderiza a página inicial com todos os tours
+ *     tags: [Views]
+ *     responses:
+ *       200:
+ *         description: Página HTML renderizada com sucesso
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ */
+
+/**
+ * @swagger
+ * /tour/{slug}:
+ *   get:
+ *     summary: Renderiza a página de detalhes de um tour pelo slug
+ *     tags: [Views]
+ *     parameters:
+ *       - in: path
+ *         name: slug
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: "Slug do tour (ex: the-forest-hiker)"
+ *     responses:
+ *       200:
+ *         description: Página HTML do tour renderizada com sucesso
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: Tour não encontrado
+ */
